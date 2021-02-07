17bd81b8be4aafe723b8c4d4b29d382235d7bf68
Christoph Korn <christoph.korn@posteo.de>
2021-02-07 21:29:07 +0100

    Add new Blazor UI
    Finally my first blazor project.
    The screenshot server can only be started from the winforms UI. So I still need that one.
    Somehow the navbar is not shown, have to fix that.

---
+++ UI/GimmeAScreenshotPleaseUI.Blazor/Program.cs
@@ -0,0 +1,38 @@
+//using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.GimmeAScreenshotPleaseUI.Blazor
+{
+    public class Program
+    {
+        //public static async Task Main(string[] args)
+        //{
+        //    var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        //    builder.RootComponents.Add<App>("#app");
+
+        //    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+        //    await builder.Build().RunAsync();
+        //}
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
