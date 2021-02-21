580ba49fb4796c5639c36b0f99429375204abf2f
Christoph Korn <christoph.korn@posteo.de>
2021-02-21 13:40:31 +0100

    Add settings for proxy support
    Unfortunately the base address the page runs in has to be hardcoded somehow.

--- UI/GimmeAScreenshotPleaseUI.Blazor/Startup.cs
+++ UI/GimmeAScreenshotPleaseUI.Blazor/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -45,6 +46,12 @@ namespace UI.GimmeAScreenshotPleaseUI.Blazor
             {
                 app.UseExceptionHandler("/Error");
             }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
 
             app.UseStaticFiles();
 
